55bd98fd197aeacc5079a086bc4d2943b9592f71
shashwat <work.shashwatojha@gmail.com>
2026-01-08 12:36:00 +0530

    translation

---
+++ basics/18translation.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+
+image = cv.imread("Photos/park.jpg")
+
+cv.imshow("park", image)
+
+# translation
+
+def translate(img, x, y):
+    transMat = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+# -x --> left
+# x --> right
+# -y --> up
+# y --> down
+
+translated = translate(image, 100, 100)
+
+cv.imshow("translated", translated)
+
+cv.waitKey(0)
